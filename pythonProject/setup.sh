5a293fb1ca314d19f804e06097ce4f13ad46115d
akshayyy22 <akshaypersonalstudentid@gmail.com>
2024-06-25 18:56:09 +0530

    2nd Commit

--- pythonProject/setup.sh
+++ pythonProject/setup.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+# Create the Streamlit configuration directory
 mkdir -p ~/.streamlit/
 
+# Create the Streamlit configuration file
 echo "\
 [server]\n\
-port = $PORT\n\
+port = \$PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+# Install required packages
+pip install -r requirements.txt
